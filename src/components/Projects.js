7c1581ccb790ef7752ba79bd933532f6ae92ec7e
Mychaela Tran <tranmychaela@gmail.com>
2025-07-18 23:36:31 -0400

    project tabs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,38 @@
 import { Container, Row, Col } from "react-bootstrap";
 import proj1 from "../assets/images/project-img1.png" //519w 512 h
-export const Projects = () =>{
-    const projects = [
-        {
-            title: "This Portfolio",
-            desc: "Personal Website Made with React",
-            image: proj1
-        }
-    ];
-    return (
-        <section className="projects" id="projects">
-            <Container>
+import { Tab, Tabs } from 'react-bootstrap';
+
+
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "This Portfolio",
+      desc: "Personal Website Made with React",
+      img: proj1
+    }
+  ];
+
+  return (
+    <section className="projects" id="projects">
+      <Container>
+        <Row>
+          <Col>
+            <Tabs defaultActiveKey="web" id="projects-tabs" className="mb-3" fill>
+              <Tab eventKey="web" title="Web Dev">
                 <Row>
-                {projects.map((project, index) => (
-                    <Col key={index} md={4}>
-                    <div className="project-card">
-                        <img src={project.image} alt={project.title} style={{ width: "100%" }} />
-                        <h4>{project.title}</h4>
-                        <p>{project.desc}</p>
-                    </div>
-                    </Col>
-                ))}
+                  {projects.map((project, index) => (
+                    <p key={index}>{project.title}</p>
+                  ))}
                 </Row>
-
-            </Container>
-        </section>
-    );
-
-}
+              </Tab>
+              <Tab eventKey="game" title="Game Dev">Game Dev Content</Tab>
+              <Tab eventKey="school" title="School Projects">School Projects</Tab>
+              <Tab eventKey="hackathons" title="Hackathons">Hackathons</Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
